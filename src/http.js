831ca0eb83a5ddd6d3afb2875bd140a7a266c77e
Denis Bakunovitch <bakunovitch@gmail.com>
2017-10-10 17:28:47 +0300

    XLSX download fix

--- src/http.js
+++ src/http.js
@@ -66,7 +66,7 @@ export default function http(url, request = {}) {
 }
 
 // exported for testing
-export const shouldDownloadAsText = (contentType = '') => /json|xml|yaml|text/.test(contentType)
+export const shouldDownloadAsText = (contentType = '') => /\bjson|xml|yaml|text\b/.test(contentType)
 
 // Serialize the response, returns a promise with headers and the body part of the hash
 export function serializeRes(oriRes, url, {loadSpec = false} = {}) {
